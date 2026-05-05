316cbc1f81347f4bff798fc412b68d1ddeea805b
diferreirasolucoes <tuctucwow31@gmail.com>
2026-05-05 20:43:25 -0300

    BGonly_1
    Mudanças no css e script

--- js/templatemo-script.js
+++ js/templatemo-script.js
@@ -15,17 +15,22 @@ const setBg = id => {
 
 const setBgOverlay = () => {
     const windowWidth = window.innerWidth;
-    const bgHeight = $('body').height();
+    const bgHeight = window.innerHeight;
     const tmBgLeft = $('.tm-bg-left');
 
     $('.tm-bg').height(bgHeight);
 
     if(windowWidth > 768) {
-        tmBgLeft.css('border-left', `0`)
-                .css('border-top', `${bgHeight}px solid transparent`);                
+        tmBgLeft.css({
+            'border-left': '0',
+            'border-top': `${bgHeight}px solid transparent`
+        });
     } else {
-        tmBgLeft.css('border-left', `${windowWidth}px solid transparent`)
-                .css('border-top', `0`);
+        // MOBILE: remove corte bugado
+        tmBgLeft.css({
+            'border-left': '0',
+            'border-top': '0'
+        });
     }
 }
 
